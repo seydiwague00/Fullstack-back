e7563ab645abf679684fbcf00089f260c31984c5
seydi <seydiwague00@gmail.com>
2025-03-19 02:28:27 +0100

    First commit

---
+++ src/main/java/com/esgitech/fsapp/FsAppApplication.java
@@ -0,0 +1,13 @@
+package com.esgitech.fsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgitech/fsapp/FsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgitech.fsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
